e970e33571257a8b607cb4cfdf3d1573d916a58f
JakeRoyRandall <jake@jakerandall.me>
2020-02-03 11:28:30 -0800

    updating to latest v feb 3 2020

---
+++ app/javascript/components/MainApp.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class MainApp extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>INDEED</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MainApp
