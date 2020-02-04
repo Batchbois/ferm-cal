97bb9bd6622d8e86c309c9c4fdcbac1b308ec2c8
brendenbarta <brendenbarta@gmail.com>
2020-02-04 11:46:17 -0800

    validations for both resources

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,8 @@
 class Batch < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :name, :ferment, :completed, :description
+
+  # def accepted_ferments
+  # end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :batch
+
+  validates_presence_of :description, :due, :completed
 end
