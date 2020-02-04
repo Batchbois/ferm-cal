13a21d85a81f291caa7cc04e90daaa75da89979e
brendenbarta <brendenbarta@gmail.com>
2020-02-04 11:47:58 -0800

    moved resource functionality below constraints in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :batches
   devise_for :users
   get '*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
+  resources :tasks
+  resources :batches
   root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
