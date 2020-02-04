3fc9aa6680219342259cc9e3c10d89faeddd5d6b
kimberlyleo <kimeleonard@gmail.com>
2020-02-04 11:18:55 -0800

    created about us component and nav bar conditionals completed

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -5,23 +5,35 @@ import {
     Navbar
 } from 'react-bootstrap';
 
-import{ Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import AboutUs from './aboutus';
 
 const Header = (props) => {
     console.log(props);
     const { current_user, signed_in, sign_in_route, sign_out_route } = props.appProps
     return(
         <Navbar bg="light" expand="sm">
-            <Navbar.Brand><Link className="navbar-brand" to= "/">Ferm-Cal</Link></Navbar.Brand>
+            <Navbar.Brand><Link className="navbar-brand" to= "/">FermCal</Link></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                   <Link className="nav-link" to="/">Home</Link>
-                  <Link className="nav-link" to="#link">Link</Link>
+                  <Link className="nav-link" to="./aboutus">About Us!</Link>
 
-                  { signed_in
-                      ? <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>
-                      : <Nav.Link href={sign_in_route}>Sign In/Sign Up</Nav.Link> }
+                  { signed_in && <Link className="nav-link" to="/tasks">Tasks</Link> }
+                  { signed_in && <Link className="nav-link" to="/active">Active</Link> }
+                  { signed_in && <Link className="nav-link" to="/archive">Archive</Link> }
+                  { signed_in && <Link className="nav-link" to="/newbatch">Create New Batch!</Link> }
+
+                  { signed_in &&
+                       <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>
+                   }
+                   {!signed_in &&
+                       <Nav.Link href={sign_in_route}>Sign In</Nav.Link>
+                   }
+                   {!signed_in &&
+                       <Nav.Link href="users/sign_up">Sign Up</Nav.Link>
+                    }
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
@@ -29,8 +41,3 @@ const Header = (props) => {
 }
 
 export default Header;
-
-//
-// { signed_in && <Link className="nav-link" to="/" /> }
-// { signed_in && <Link className="nav-link" to="/" /> }
-// { signed_in && <Link className="nav-link" to="/" /> }
---
+++ app/javascript/components/landingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotSignedInLanding = () => {
+    return(
+        <h1> WELCOME TO FERMCAL
+            <br/>
+            SIGN UP AND HAVE A BATCHIN' GOOD TIME
+        </h1>
+    )
+}
+
+export default NotSignedInLanding;
