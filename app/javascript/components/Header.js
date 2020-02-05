e3f28687cfa35b5c09233864f052abf4b4c8cf2c
kimberlyleo <kimeleonard@gmail.com>
2020-02-05 12:12:52 -0800

    fixed nav bar and switched from react bootstrap to reactstrap it now has functional hamburger

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,110 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import AboutUs from './pages/aboutus';
 
 
 import {
-    Nav,
-    Button,
-    Navbar
-} from 'react-bootstrap';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
 
 
 const Header = (props) => {
     const { current_user, signed_in, sign_in_route, sign_out_route } = props.appProps
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-          <Link className="navbar-brand" to="/">FermCal</Link>
-          <Button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </Button>
-
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="./aboutus">About Us</Link>
-              </li>
-              <li className="nav-item">
-              { signed_in && <Link className="nav-link" to="/tasks">Tasks</Link> }
-              </li>
-              <li className="nav-item">
-              { signed_in && <Link className="nav-link" to="/active">Active</Link> }
-              </li>
-              <li className="nav-item">
-              { signed_in && <Link className="nav-link" to="/archive">Archive</Link> }
-              </li>
-              <li className="nav-item">
-              { signed_in && <Link className="nav-link" to="/newbatch">Create New Batch!</Link> }
-              </li>
-              <li className="nav-item">
-              { signed_in && <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>}
-              </li>
-              <li className="nav-item">
-              { !signed_in && <Nav.Link href={sign_in_route}>Sign In</Nav.Link>}
-              </li>
-              <li className="nav-item">
-              { !signed_in && <Nav.Link href="users/sign_up">Sign Up</Nav.Link>}
-              </li>
-            </ul>
-
-          </div>
-        </nav>
-
-
-
-
-
+        <div>
+            <Navbar className="navbar navbar-expand-sm navbar-dark bg-success">
+              <NavbarBrand href="/">Ferm-Cal</NavbarBrand>
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                  <NavItem>
+                    <Link className="nav-link" to="/aboutus/">About us!</Link>
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to= "/tasks">Tasks</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to="/active">Active</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to="/achive">Archive</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to="/newbatch">New Batch!</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to={sign_out_route}>Sign Out</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {signed_in && <Link className="nav-link" to={sign_in_route}>Sign In</Link>}
+                  </NavItem>
+                  <NavItem>
+                    {!signed_in && <Link className="nav-link" to="users/sign_up">Sign Up</Link>}
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+        </div>
 
     )
 }
 
 export default Header;
-
-
-// <div className="nav">
-// <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
-// <Link className="navbar-brand" href="/">FermCal</Link>
-//
-//
-// <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-//
-//
-//
-// <ul className="navbar-nav mr-auto">
-// <li className="nav-item">
-// <Link className="nav-link" href="/">Home <span className="sr-only">(current)</span></Link>
-// </li>
-// <li className="nav-item">
-// <Link className="nav-link" href="#">About Us!</Link>
-// </li>
-// <li className="nav-item">
-// { signed_in && <Link className="nav-link" to="/tasks">Tasks</Link> }
-// </li>
-// <li className="nav-item">
-// { signed_in && <Link className="nav-link" to="/active">Active</Link> }
-// </li>
-// <li className="nav-item">
-// { signed_in && <Link className="nav-link" to="/archive">Archive</Link> }
-// </li>
-// <li className="nav-item">
-// { signed_in && <Link className="nav-link" to="/newbatch">Create New Batch!</Link> }
-// </li>
-// <li className="nav-item">
-// { signed_in && <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>}
-// </li>
-// <li className="nav-item">
-// { !signed_in && <Nav.Link href={sign_in_route}>Sign In</Nav.Link>}
-// </li>
-// <li className="nav-item">
-// { !signed_in && <Nav.Link href="users/sign_up">Sign Up</Nav.Link>}
-// </li>
-// </ul>
-// </div>
-//
-// </Navbar.Toggle>
-// </Navbar>
-// </div>
