169347c94c6198558cce2814a1395ac08f9adc98
kimberlyleo <kimeleonard@gmail.com>
2020-02-06 13:21:46 -0800

    form for new batch improved and calendar imported from react-calendar

--- app/javascript/components/MainApp.js
+++ app/javascript/components/MainApp.js
@@ -12,32 +12,35 @@ import AboutUs from "./pages/aboutus";
 import Dashboard from "./pages/dashboard";
 import BatchShow from "./pages/batchshow";
 import CreateNewBatch from "./pages/createnewbatch";
+import Calendar from "react-calendar";
 // import 'bootstrap/dist/js/bootstrap.min.js';
 // when this is included,page doesnt render
 
 
 class MainApp extends React.Component {
+    
+    render () {
+        const { signed_in } = this.props
+
+        return (
+          <Router>
+            <Header appProps={this.props}/>
+            {!signed_in &&
+            <NotSignedInLanding />}
 
 
-  render () {
-    const { signed_in } = this.props
-    return (
-      <Router>
-        <Header appProps={this.props}/>
-        {!signed_in &&
-        <NotSignedInLanding />}
+
+            <Switch>
+                <Route exact path= "/" component={NotSignedInLanding}/>
+                <Route path="/aboutus" component={AboutUs} />
+                <Route path="/dashboard" component={Dashboard}/>
+                <Route path="/batchshow" component={BatchShow}/>
+                <Route path="/newbatch" component={CreateNewBatch}/>
 
 
-        <Switch>
-            <Route exact path= "/" component={NotSignedInLanding}/>
-            <Route path="/aboutus" component={AboutUs} />
-            <Route path="/dashboard" component={Dashboard}/>
-            <Route path="/batchshow" component={BatchShow}/>
-            <Route path="/newbatch" component={CreateNewBatch}/>
 
-
-        </Switch>
-      </Router>
+            </Switch>
+          </Router>
     );
   }
 }
