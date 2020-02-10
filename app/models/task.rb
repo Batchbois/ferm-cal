d3a1945986f6b27f98a05bba70b32024b1cfc509
brendenbarta <brendenbarta@gmail.com>
2020-02-10 13:10:15 -0800

    testing task_spec.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :batch
+  belongs_to :batch 
 
   validates_presence_of :title, :description, :due
   validates :completed, inclusion: { in: [true, false] }
