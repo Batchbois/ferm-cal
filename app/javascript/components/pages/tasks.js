9a0b756f84cc0ad64907aa96c7747ee7e2b26038
kimberlyleo <kimeleonard@gmail.com>
2020-02-13 16:33:08 -0800

    changed color to improve readability

--- app/javascript/components/pages/tasks.js
+++ app/javascript/components/pages/tasks.js
@@ -10,7 +10,7 @@ const Tasks = (props) => {
     let { tasks } = props
     return(
         <Container>
-            <Card className="card text-warning bg-warning mb-3" >
+            <Card className="card text-secondary bg-secondary mb-3" >
                 <CardBody>
                     <CardTitle className="card-title text-white"><h2>Upcoming Tasks</h2></CardTitle>
                         <ul className="list-group list-group-flush" id= "tasks-upcoming">
